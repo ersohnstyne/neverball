bc629b0410d7971c5d49228eddb5011605c7fb52
ersohnstyne <ersohn.styne@gmail.com>
2025-02-12 11:23:03 +0100

    Update: Updated source copyright

--- share/lang.h
+++ share/lang.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2024 Microsoft / Neverball authors
+ * Copyright (C) 2025 Microsoft / Neverball authors / Jānis Rūcis
  *
  * this file is  free software; you can redistribute  it and/or modify
  * it under the  terms of the GNU General  Public License as published
